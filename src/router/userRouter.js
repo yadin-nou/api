33dca8172d4eeab0f3e62a428f266d285483c907
Yadin Nou <nouyadin@gmail.com>
2026-08-07 07:28:18 +1000

    cleanup code and prepare for Mongo DB connection

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
 const userRouter = express.Router();
-let fakeDB = [];
+//let fakeDB = [];
 userRouter.get("/", (req, res) => {
   res.status(201).json({
-    task: fakeDB,
+    task: [],
     sucess: "sucesss",
     message: "To do Get",
   });
@@ -11,7 +11,7 @@ userRouter.get("/", (req, res) => {
 
 userRouter.post("/", (req, res) => {
   //const taskList = { id: fakeDB.length + 1, ...req.body };
-  fakeDB.push(req.body);
+  //fakeDB.push(req.body);
   res.status(201).json({
     sucess: "sucess",
     message: "Task added",
@@ -21,8 +21,8 @@ userRouter.post("/", (req, res) => {
 userRouter.delete("/{:id}", (req, res) => {
   //+ convert from string to number
   const id = +req.params.id;
-  fakeDB = fakeDB.filter((task) => task.id !== id);
-  console.log(fakeDB, id);
+  // fakeDB = fakeDB.filter((task) => task.id !== id);
+  //console.log(fakeDB, id);
   res.status(201).json({
     sucess: "sucess",
     message: "delete sucessfully",
@@ -30,10 +30,10 @@ userRouter.delete("/{:id}", (req, res) => {
 });
 
 userRouter.patch("/", (req, res) => {
-  fakeDB = fakeDB.filter((item) => {
-    return item.id === +req.body.id ? (item.type = req.body.type) : item;
-  });
-  console.log(fakeDB);
+  // fakeDB = fakeDB.filter((item) => {
+  //   return item.id === +req.body.id ? (item.type = req.body.type) : item;
+  // });
+  // console.log(fakeDB);
   res.status(201).json({
     sucess: "sucess",
     message: "task updated",
