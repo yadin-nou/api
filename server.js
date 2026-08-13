ab61aabfc7c35b83537d1c3b25dfd884b5afa112
Yadin Nou <nouyadin@gmail.com>
2026-08-14 06:25:03 +1000

    update path

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import userRouter from "./src/router/userRouter.js";
 import morgan from "morgan";
 import { connectMongoDB } from "./src/config/db.Config.js";
 import cors from "cors";
+
 const app = express();
 const PORT = 8000;
 //MongoDB connection
 connectMongoDB();
 // use cors to tell server that we can send data through
+//OR to open permersion for data sending too.
 app.use(cors());
 //morgan to tell http reqest path you send in console
 app.use(morgan("tiny"));
@@ -15,8 +17,19 @@ app.use(morgan("tiny"));
 app.use(express.json());
 //use for form submit req.body
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/v1/tasks", userRouter);
+//static servering path
+import path from "path";
+const __dirName = path.resolve();
+//serve the static file for dist folder (react build folder)
+const joinFile = path.join(__dirName, "dist");
+app.use(express.static(joinFile));
+//start run static file in dist folder of react
+app.get("/", (req, res) => {
+  res.sendFile(joinFile, "index.html");
+});
 
+//user express router
+app.use("/api/v1/tasks", userRouter);
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log("http://localhost:" + PORT);
 });
